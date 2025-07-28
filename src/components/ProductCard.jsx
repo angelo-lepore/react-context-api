cae96ef2fade22271742500285c16add10751f22
angelo-lepore <angelo.lepore94@gmail.com>
2025-07-28 17:04:17 +0200

    Refactor ProductCard component to destructure product properties for cleaner code

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,31 +3,31 @@ import { Link } from "react-router-dom";
 
 // Componente che mostra una singola "card" prodotto
 export default function ProductCard({ product }) {
-  const { id, title, price, image, category } = product;
-
   return (
     <>
       <div className="card h-100 border border-dark bg-white text-dark shadow">
-        <Link to={`/products/${id}`}>
+        <Link to={`/products/${product?.id}`}>
           <img
-            src={image}
+            src={product?.image}
             className="card-img-top p-3"
-            alt={title}
+            alt={product?.title}
             style={{ objectFit: "contain", height: "200px" }}
           />
         </Link>
         <div className="card-body d-flex flex-column">
           <Link
-            to={`/products/${id}`}
+            to={`/products/${product?.id}`}
             className="text-decoration-none text-dark"
           >
-            <h6 className="card-title fw-semibold">{title}</h6>
+            <h6 className="card-title fw-semibold">{product?.title}</h6>
           </Link>
           <p className="card-text text-muted text-capitalize small mb-2">
-            {category}
+            {product?.category}
           </p>
           <div className="mt-auto d-flex justify-content-between align-items-center">
-            <span className="fw-bold text-dark">${price?.toFixed(2)}</span>
+            <span className="fw-bold text-dark">
+              ${product?.price?.toFixed(2)}
+            </span>
             <button className="btn btn-outline-dark btn-sm">
               Aggiungi al carrello <i className="bi bi-cart-plus"></i>
             </button>
